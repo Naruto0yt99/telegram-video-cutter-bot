2bf6cb50267cc5db5117d3720305b9afe43b91c8
Naruto0yt99 <tiktokeditor6162@gmail.com>
2026-09-19 22:59:42 +0530

    Bump source parser version to 25

--- source_sync.py
+++ source_sync.py
@@ -10,7 +10,7 @@ from telegram_media import is_video_message, get_message_video_name
 from library_nav import canonical_anime
 
 logger = logging.getLogger("anime-bot.source-sync")
-PARSER_VERSION = 24
+PARSER_VERSION = 25
 
 
 _QUALITY_PATTERNS = [
